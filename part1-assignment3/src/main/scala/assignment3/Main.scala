aba72df928395cf77da770c42d656d555feb97c3
nemanja-stanarevic <nemanja.stanarevic@gmail.com>
2014-11-10 15:33:41 -0500

    Refactored assignment 3 a bit

--- part1-assignment3/src/main/scala/assignment3/Main.scala
+++ part1-assignment3/src/main/scala/assignment3/Main.scala
@@ -6,80 +6,68 @@ import scala.annotation.tailrec
 import java.lang.Boolean
 
 object Main extends App {
-  case class Graph[T](
-    vertices: Vector[T],
+  case class Graph(
+    vertices: Vector[Set[Int]],
     edges: Vector[(Int, Int)])
 
-  def loadGraph(fileName: String): Graph[Set[Int]] =
+  def loadGraph(fileName: String): Graph =
     parseGraph(scala.io.Source.fromFile(fileName).mkString)
 
-  def parseGraph(graphStr: String): Graph[Set[Int]] = 
+  def parseGraph(graphStr: String): Graph =
     graphStr
       .lines
-      .foldLeft(Graph[Set[Int]](Vector(), Vector())) {
+      .foldLeft(Graph(Vector(), Vector())) {
         (graph, inputLine) ⇒
           val list = inputLine.split(Array(' ', '\t', ',')).foldLeft(Vector[Int]()) {
             (v, vertexStr) ⇒ v :+ vertexStr.toInt
           }
           val vertex = list.head
           val adjecency = list.tail
-          // -1s to adjust for zero-based vector indexing 
-          val edges = adjecency map { destVertex ⇒ (vertex - 1, destVertex - 1) }
+          val edges = adjecency map { destVertex ⇒ (vertex, destVertex) }
           Graph(
             graph.vertices :+ Set(vertex),
             graph.edges ++ edges)
       }
 
-  @tailrec def ranomizedContraction(graph: Graph[Set[Int]])
-    (implicit rng: Random): Graph[Set[Int]] =
+  @tailrec def ranomizedContraction(graph: Graph)(implicit rng: Random): Graph =
     if (graph.vertices.size <= 2)
       graph
     else {
       // pick an edge at random
       val (v1, v2) = graph.edges(rng.nextInt(graph.edges.size))
+      val v1index = graph.vertices indexWhere (_ contains v1)
+      val v2index = graph.vertices indexWhere (_ contains v2)
+      // contract v1 and v2 into a single vertex
+      val contractedVertex = graph.vertices(v1index) ++ graph.vertices(v2index)
       val newVertices = graph.vertices
-        // contract v1 and v2 into a single vertex
-        .updated(v1, graph.vertices(v1) ++ graph.vertices(v2))
-        // remove v2
-        .patch(from = v2, patch = Nil, replaced = 1)
-      val newEdges = graph.edges
-        // Contract the edges that connect the two vertices
-        .map {
-          case (x, y) if (x == v2) ⇒ (v1, y)
-          case (x, y) if (y == v2) ⇒ (x, v1)
-          case edge                ⇒ edge
-        }
-        // Update edges with index greater than v2
-        .map {
-          case (x, y) if (x > v2) & (y > v2) ⇒ (x - 1, y - 1)
-          case (x, y) if (y > v2)            ⇒ (x, y - 1)
-          case (x, y) if (x > v2)            ⇒ (x - 1, y)
-          case edge                          ⇒ edge
-        }
-        // Remove loops
-        .filterNot { edge ⇒ edge._1 == edge._2 }
+        .updated(v1index, contractedVertex)
+        .patch(from = v2index, patch = Nil, replaced = 1)
+      // Filter out any loops
+      val newEdges = graph.edges.filterNot {
+        case (from, to) ⇒
+          (contractedVertex contains from) && (contractedVertex contains to)
+      }
       // construct a new graph and iterate
       ranomizedContraction(Graph(newVertices, newEdges))
     }
 
-  def countCuts[T](graph: Graph[T]): Int =
+  def countCuts[T](graph: Graph): Int =
     graph.edges count {
-      case (0, 1) ⇒ true
-      case _      ⇒ false
+      case (x, y) ⇒ (graph.vertices.head contains x) & !(graph.vertices.head contains y)
     }
 
-  def calculateMinCuts(graph: Graph[Set[Int]], debug: Boolean = false): Int = {
+  def calculateMinCuts(graph: Graph, debug: Boolean = false): Int = {
     val n = graph.vertices.size
-    val iterations = ( Math.pow(n, 2) * Math.log(n) ).toInt
+    val iterations = (Math.pow(n, 2) * Math.log(n)).toInt
     if (debug)
       println(s"Looking for min cut in {$iterations} iterations")
 
+    implicit val rng = new Random()
     var minCuts = Int.MaxValue
-    for (iter <- 0 until iterations) {
+    for (iter ← 0 until iterations) {
+      rng.setSeed(iter)
       if (debug & (iter % 100 == 0))
         println(s"Iteration #{$iter}...")
-
-      implicit val rng = new Random()
       val cuts = countCuts(ranomizedContraction(graph))
       if (cuts < minCuts) {
         minCuts = cuts
